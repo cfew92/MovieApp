472147315757c2cb5f8d95f74280f3bc333686eb
cfew92 <82827339+cfew92@users.noreply.github.com>
2021-06-17 18:44:26 -0500

    creating a new app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieEditComponent } from './movie-edit/movie-edit.component';
+import { HeaderComponent } from './header/header.component';
+import { ReviewEditComponent } from './review-edit/review-edit.component';
+import { ReviewListComponent } from './review-list/review-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MovieEditComponent,
+    HeaderComponent,
+    ReviewEditComponent,
+    ReviewListComponent
   ],
+
+
   imports: [
     BrowserModule,
     AppRoutingModule
